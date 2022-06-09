c09f64613371730e4318015b383b8afc98dd5864
Michael Kassim <master_kunle14@hotmail.co.uk>
2022-06-09 14:41:43 +0100

    set up application binary interface and binary variables

--- deploy.js
+++ deploy.js
@@ -1,3 +1,5 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
 //synchronus [solidity]
 //asychronus [javascript]
 
@@ -10,9 +12,18 @@
 //Rejected
 
 async function main() {
-  console.log("hi");
-  let variable = 5;
-  console.log(variable);
+  const provider = new ethers.providers.JsonRpcProvider(
+    "HTTP://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "cd602cd7d69ef94877ba54f6bd97e6c56357d56680cd0154932fab04e86ccd44",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
 }
 
 main()
